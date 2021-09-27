3e3722ae9494c6f00636f959688d04d26c1645bd
1Soyebo <soyeboibukun@gmail.com>
2021-09-27 19:27:14 +0100

    initial commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Ibukunoluwa Soyebo on 27/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
